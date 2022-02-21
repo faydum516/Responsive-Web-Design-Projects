fccab2b978f9b782a6c94b8eed4123f597c4894c
Hugo <hugoxu11@gmail.com>
2022-02-21 12:58:52 -0500

    new edit

--- geographical_map/geographical_map.js
+++ geographical_map/geographical_map.js
@@ -4,4 +4,4 @@ function input() {
 	x = document.getElementById("input").value;
 	link = `https://maps.google.com/maps?q=${x.replace(/ /g, '%20')}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
 	document.getElementById("gmap_canvas").src = link;
-			}
+}
