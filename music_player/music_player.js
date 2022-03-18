330cd2f21d921535a815f377c41599cf037dcd03
Hugo <hugoxu11@gmail.com>
2022-03-17 21:47:58 -0400

    new commit

--- music_player/music_player.js
+++ music_player/music_player.js
@@ -1,18 +1,28 @@
-function play(source, image, alt) {
-    document.querySelector('#songcover').src = image;
-    document.querySelector('#songcover').alt = alt;
-    document.querySelector('#audio-source').src = source;
-    document.querySelector('#musicplay').style.display = 'flex';
-}
-
 fetch("songlist.json")
     .then(response => response.json())
     .then(data => {
-        let songSection = document.querySelector('#songsection');
-        let songList = document.createElement('ul');
+        const body = document.querySelectorAll("body")[0];
+
+        const songCover =  document.querySelector('#songcover');
+        const music = document.querySelector('#audio-source');
+        const musicPlay = document.querySelector('#musicplay');
+
+        const leftarrow = document.querySelector('#left-arrow');
+        const rightarrow = document.querySelector('#right-arrow');
+
+        const songTitle = document.querySelector('#song-name');
+        const musician = document.querySelector('#artist-name');
+
+        const songSection = document.querySelector('#songsection');
+        const songList = document.createElement('ul');
         songList.className = "songlist";
         songSection.appendChild(songList);
 
+        const backToMenu = document.querySelector('#backToMenu');
+        const backToPlay = document.querySelector('#backToPlay');
+
+        const musicSearch = new Map();
+        var id, index, currentSong;
 
         for (let obj of data) {
             var songItem = document.createElement('li');
@@ -29,7 +39,105 @@ fetch("songlist.json")
             songItem.appendChild(songInfo);
             songItem.appendChild(artistInfo);
 
-            songItem.setAttribute('onclick', `play("${obj.source}", "${obj.cover}", "${obj.alt}")`); // The setAttribute() method sets a new value to an attribute. 
+            songItem.onclick = function() {
+                id = obj.id;
+                index = id - 1;
+                songCover.src = obj.cover;
+                songCover.alt = obj.alt;
+                music.src = obj.source;
+                songTitle.innerHTML = obj.song;
+                musician.innerHTML = obj.artist;
+                musicPlay.style.display = "flex";
+                backToMenu.style.display = "inline-block";
+                songSection.style.display = "none";
+                backToPlay.style.display = "none";
+                
+                if (currentSong !== undefined) {
+                    currentSong.style.setProperty('background-color', 'initial');
+                }
+                currentSong = document.querySelectorAll(".song")[index];
+                currentSong.style.backgroundColor = "hsl(238, 23%, 15%)";
+
+                body.style.height = "100vh";
+            }
+
+            musicSearch.set(obj.id, obj);
             songList.appendChild(songItem);
+        }
+
+        music.onended = function() {
+            id++;
+            if (id > data.length) {
+                id = 1;
+            }
+            index = id - 1;
+            music.pause();
+            songCover.src = musicSearch.get(id).cover;
+            songCover.alt = musicSearch.get(id).alt;
+            music.src = musicSearch.get(id).source;
+            songTitle.innerHTML = musicSearch.get(id).song;
+            musician.innerHTML = musicSearch.get(id).artist;
+            music.load();
+            music.play();
+            
+            if (currentSong !== undefined) {
+                currentSong.style.setProperty('background-color', 'initial'); // The setProperty() method sets a new or modifies an existing CSS property in a CSS declaration block. Setting a property to ‘initial’ resets the property to its initial value, removing any effect of the property.
+            }
+            currentSong = document.querySelectorAll(".song")[index];
+            currentSong.style.backgroundColor = "hsl(238, 23%, 15%)";
+        }
+
+        leftarrow.onclick = function() {
+            id--;
+            if (id < 1) {
+                id = data.length;
+            }
+            index = id - 1;
+            songCover.src = musicSearch.get(id).cover;
+            songCover.alt = musicSearch.get(id).alt;
+            music.src = musicSearch.get(id).source;
+            songTitle.innerHTML = musicSearch.get(id).song;
+            musician.innerHTML = musicSearch.get(id).artist;
+
+            if (currentSong !== undefined) {
+                currentSong.style.setProperty('background-color', 'initial'); // The setProperty() method sets a new or modifies an existing CSS property in a CSS declaration block. Setting a property to ‘initial’ resets the property to its initial value, removing any effect of the property.
+            }
+            currentSong = document.querySelectorAll(".song")[index];
+            currentSong.style.backgroundColor = "hsl(238, 23%, 15%)";
+        }
+
+        rightarrow.onclick = function() {
+            id++;
+            if (id > data.length) {
+                id = 1;
+            }
+            index = id - 1;
+            songCover.src = musicSearch.get(id).cover;
+            songCover.alt = musicSearch.get(id).alt;
+            music.src = musicSearch.get(id).source;
+            songTitle.innerHTML = musicSearch.get(id).song;
+            musician.innerHTML = musicSearch.get(id).artist;
+            
+            if (currentSong !== undefined) {
+                currentSong.style.setProperty('background-color', 'initial'); // The setProperty() method sets a new or modifies an existing CSS property in a CSS declaration block. Setting a property to ‘initial’ resets the property to its initial value, removing any effect of the property.
+            }
+            currentSong = document.querySelectorAll(".song")[index];
+            currentSong.style.backgroundColor = "hsl(238, 23%, 15%)";
+        }
+
+        backToMenu.onclick = function() {
+            songSection.style.display = "flex";
+            musicPlay.style.display = "none";
+            backToMenu.style.display = "none"
+            backToPlay.style.display = "inline-block";
+            body.style.height = "100%";
+        }
+
+        backToPlay.onclick = function() {
+            songSection.style.display = "none";
+            musicPlay.style.display = "flex";
+            backToMenu.style.display = "inline-block";
+            backToPlay.style.display = "none";
+            body.style.height = "100vh";
         }
     });
